a6af85e635b3b40ed389e9b3269f7ab4968d792e
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2023-05-05 01:18:53 +0530

    external ImageViewer added

---
+++ src/com/company/external/ImageViewer.java
@@ -0,0 +1,12 @@
+package com.company.external;
+
+//assume that this class was written by someone else and you can't change it.
+public class ImageViewer {
+
+    public void show(String filePath){
+        System.out.println("Showing the image using ImageViewer " + filePath);
+    }
+}
+
+//This class is incompatible with Player interface and also you can't modify this class.
+//wall socket example
